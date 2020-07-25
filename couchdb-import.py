742eb4dff350981cd6683ac25b78987b92c72daa
Vladimir Hidalgo <vladimir@2600hz.com>
2020-07-24 18:54:03 -0600

    Significatively improvement of import times, bulk_docs when applicable

--- couchdb-import.py
+++ couchdb-import.py
@@ -9,6 +9,49 @@ import shutil
 import glob
 import time
 import gc
+import cloudant
+
+### Functions
+def get_json_documents(lines):
+    documents = []
+    for line in lines:
+        documents.append(json.loads(line.strip()))
+    return documents
+    
+def db_has_attachments(documents):
+    for document in documents:
+        if '_attachments' in document:
+            return True
+    return False
+
+def sequential_import(database, document_json):
+    print ("  ...Doing sequential import because this DB contains attachments")
+    for document_json in documents:
+        attachments = document_json.get('_attachments', None)
+        document_json.pop('_attachments', None)
+        try:
+            document = database.create_document(document_json)            
+        except OSError as err:
+            print ("Bad document or view found. {}/{} - {}".format(database_name, document_json['_id'], err))
+        
+        del document_json
+
+        if attachments:
+            print ("  ...Importing attachments for document " + document['_id'])
+            for attachment in attachments:
+                attachment_hashed = hashlib.sha1(attachments[attachment]['digest'].encode('utf-8')).hexdigest()
+                print('    -> {} - {}'.format(attachment, attachment_hashed))
+                file_attachment = open(path_attachments + attachment_hashed, "rb")
+                document.put_attachment(attachment, attachments[attachment]['content_type'], file_attachment.read())
+                file_attachment.close()
+        ### if attachments
+        document.clear()
+    ### for line
+
+### Functions
+
+def bulk_import(database, documents):
+    database.bulk_docs(documents)
 
 parser = argparse.ArgumentParser(description='Dumps CouchDB / Bigcouch databases')
 parser.add_argument('--host', help='FQDN or IP, including port. Default: http://localhost:5984', default='http://localhost:5984')
@@ -53,37 +96,27 @@ for file in files:
     filehandle = open(file, 'r') 
     lines = filehandle.readlines() 
     database_name = lines[0].strip()
-    print("Importing DB: {} - {}".format(database_name, file))    
+    print("Importing DB: {} - {}".format(database_name, os.path.basename(file)))    
     if args.delete:
-        client.delete_database(database_name)
-        time.sleep(1)
+        try:
+            client.delete_database(database_name)
+            time.sleep(1)
+        except cloudant.error.CloudantClientException:
+            pass
 
     database = client.create_database(database_name)
     
     # From line 1 (starting at 0), are all documents.
-    for line in lines[1:]:
-        document_json = json.loads(line.strip())
-        attachments = document_json.get('_attachments', None)
-        document_json.pop('_attachments', None)
-        try:
-            document = database.create_document(document_json)            
-        except OSError as err:
-            print ("Bad document or view found. {}/{} - {}".format(database_name, document_json['_id'], err))
-        
-        del document_json
+    documents = get_json_documents(lines[1:])
+    
+    if db_has_attachments(documents):
+        sequential_import(database, documents)
+    else:
+        bulk_import(database, documents)
+    
+    del lines, documents
 
-        if attachments:
-            print ("  ...Importing attachments for document " + document['_id'])
-            for attachment in attachments:
-                attachment_hashed = hashlib.sha1(attachments[attachment]['digest'].encode('utf-8')).hexdigest()
-                print('    -> {} - {}'.format(attachment, attachment_hashed))
-                file_attachment = open(path_attachments + attachment_hashed, "rb")
-                document.put_attachment(attachment, attachments[attachment]['content_type'], file_attachment.read())
-                file_attachment.close()
-        ### if attachments
-        document.clear()
-    ### for line
-    del lines
     database.clear()
+    client.clear()
     filehandle.close()
-### for file    
+### for file
